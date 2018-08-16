d10e0911982553b7b1ab67edfcb601c43e46d969
Artur Diniz Adam <artur.daz@gmail.com>
2018-08-16 15:54:52 -0300

    Medindo performance do update na classe View

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -5,12 +5,19 @@ export abstract class View<T>{
     constructor(seletor: string, private _safe: boolean = true){
         this._elemento = $(seletor)
     }
+
     update(model: T): void{
+        const tempoInicial = performance.now()
+
         const template = this._safe
             ? this.template(model).replace(/<script>[\s\S]*?<\/script>/g, '')
             : this.template(model)
 
         this._elemento.html(template)
+
+        const tempoFinal = performance.now()
+
+        console.log(`Tempo de execução de update: ${tempoFinal - tempoInicial} ms`)
     }
 
     abstract template(model: T): string
